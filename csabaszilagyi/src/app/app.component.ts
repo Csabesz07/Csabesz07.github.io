d3f74d4ce24233c8fc5b17e4d458897ecb51b298
Csaba Szilagyi <csabi32105@gmail.com>
2025-02-21 21:07:55 +0100

    Added translate module

--- csabaszilagyi/src/app/app.component.ts
+++ csabaszilagyi/src/app/app.component.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TranslateModule } from "@ngx-translate/core";
+import { TranslateService } from "@ngx-translate/core";
 
 @Component({
     selector: 'app-root',
-    imports: [RouterOutlet],
+    imports: [
+      RouterOutlet,
+      TranslateModule,
+    ],
     templateUrl: './app.component.html',
     styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'csabaszilagyi';
+  constructor(private translate: TranslateService) {
+    this.translate.addLangs(['hu', 'en']);
+    this.translate.setDefaultLang('en');
+    this.translate.use('en');
+  }
 }
